f52aa1eeeae28343cac661daf8110a01aa795f0d
Kerros13 <73565783+Kerros13@users.noreply.github.com>
2020-12-14 18:23:48 -0600

    Multi-select
    Estamamos intententando have un multi-select

--- src/screens/imagesbyAlbumScreen.js
+++ src/screens/imagesbyAlbumScreen.js
@@ -1,19 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import { StyleSheet,Dimensions,FlatList,StatusBar,Image } from "react-native";
+import { StyleSheet,Dimensions,FlatList,StatusBar,Image,Modal } from "react-native";
 import { Ionicons,Entypo } from '@expo/vector-icons';
-import {
-  Container,
-  Content,
-  Text,
-  Textarea,
-  Spinner,
-  Button,
-  View,
-  Card
-} from "native-base";
+import { Container,Content,Text,Textarea,Spinner,Button,View,Card } from "native-base";
+import CheckBox from '@react-native-community/checkbox'
+import Images from 'react-native-scalable-image';
 import * as Font from "expo-font";
-//import Image from 'react-native-scalable-image'
 
 // Importar el contexto de las notas
 import { ImagesContext } from "../context/ImagesContext";
@@ -23,8 +15,11 @@ const {width, height} = Dimensions.get("window");
 const ImagesByAlbumScreen = ({ route, navigation }) => {
   const { id,name } = route.params;
   const [theImages, setTheImages] = useState(null);
+  const [open,setOpen] = useState(false);
   const imagesContext = useContext(ImagesContext);
   const { image, getImageByAlbumId } = imagesContext;
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  const [data, setData] = useState(null)
 
   //Arreglo de imagenes original
   const { images } = useContext(ImagesContext);
@@ -42,6 +37,27 @@ const ImagesByAlbumScreen = ({ route, navigation }) => {
 
   }, []);
 
+  
+  const _onValueChange = (item, index) => {
+    const newData = data.map(e => {
+      if (e.id == item.id) {
+        return {
+          ...e,
+          seleted: !e.seleted
+        }
+      }
+      return {
+        ...e,
+        seleted: e.seleted
+      }
+    });
+    console.log('AAAA:', newData)
+    this.setState({
+      data: newData
+    })
+  }
+
+
 //   const handlerSaveNote = async () => {
 //     // Validar que la nota tiene valor
 //     if (note) {
@@ -53,7 +69,12 @@ const ImagesByAlbumScreen = ({ route, navigation }) => {
 //       setErrorNote(true);
 //     }
 //   };
-
+const handlerCheckBox = () =>{
+  if(toggleCheckBox === true)
+    setToggleCheckBox(false)
+  else
+    setToggleCheckBox(true)
+}
 
   return (
     <Container >
@@ -62,15 +83,15 @@ const ImagesByAlbumScreen = ({ route, navigation }) => {
           <View style={styles.square}/>
         </View>
         <View>
-          <View style={{position:'absolute', left:width*0.06, top:height*0.027}}>
-          <Ionicons name="ios-arrow-back" size={30} color="#3c1e22" onPress= {()=> navigation.goBack()} />  
+          <View style={{position:'absolute', left:width*0.05, top:height*0.01}}>
+            <Ionicons style={{padding:10}} name="ios-arrow-back" size={30} color="#3c1e22" onPress= {()=> navigation.goBack()} />  
           </View>
           <View style={{position:'absolute', left:width*0.25, top:height*0.027}}>
             <Text style={{fontSize:20,color:'#3c1e22'}}>{name}</Text>
           </View>
-          {id!=1?<View style={{position:'absolute', right:width*0.06, top:height*0.027}}>
-                  <Ionicons name="ios-add" size={35} style={{position:'absolute',right:5}} color="black"/>
-                </View>:null} 
+          {id==1 ? <View style={{position:'absolute', right:width*0.06, top:height*0.027}}>
+                  <Ionicons name="ios-add" size={35} style={{position:'absolute',right:5}} color="black" onPress={() => setOpen(true)}/>
+                </View>:null}
           
         </View>
             {id==1 ?
@@ -123,6 +144,39 @@ const ImagesByAlbumScreen = ({ route, navigation }) => {
                 }
             }
             />}
+            
+            <Modal animationType="fade" transparent={true} visible={open}>
+                <StatusBar backgroundColor="#000000aa" />
+                <Ionicons name="ios-arrow-back" size={35} color="black" style={{position:'absolute', zIndex:2,margin:5,padding:15}} onPress={()=>setOpen(false)} />
+                <View style={{backgroundColor:"#fff",flex:1, justifyContent:"center", alignItems:"center"}}>
+                <FlatList showsVerticalScrollIndicator={false}
+                  horizontal={false}
+                  numColumns={3}
+                  style={{borderRadius:1,marginTop:67}}
+                  data={images}
+                  keyExtractor={(item)=>item.id.toString()}
+                  ListEmptyComponent={<View style={{flex:1, alignItems:"center",justifyContent:"center"}}>
+                                        <Text>No hay fotos para eliminar</Text>
+                                      </View>}
+
+                  renderItem={({item, index}) => {
+                          return(
+                              <View>
+                                <CheckBox
+                                  disabled={false}
+                                  value={toggleCheckBox[index]}
+                                  onValueChange={(newValue) => setToggleCheckBox(newValue)}
+                                />
+                                <View style={{flex:1, alignItems:"center",marginTop:0}}>
+                                  <Images  source={{uri : item.image}} width={width/3}/>
+                                </View>
+                              </View>
+                          )
+                      }
+                  }
+                />
+                </View>
+            </Modal>
             
     </Container>
   );
